08d5015c4a855422b4f50df44de57b66bf6b369c
ahmedrabie <ahmedelezmazy36@gmail.com>
2026-03-06 20:38:57 +0200

    version

---
+++ ssl/generate_ssl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p certs
+
+openssl req -x509 -nodes -days 365 \
+-newkey rsa:2048 \
+-keyout certs/key.pem \
+-out certs/cert.pem \
+-subj "/C=US/ST=State/L=City/O=Dev/CN=localhost"
+
+echo "SSL certificate generated!"
